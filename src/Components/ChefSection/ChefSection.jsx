21074a5419848edf6c7a2ec4f7e74229f595b040
SMTamim <tamimmahmud0@gmail.com>
2023-05-16 00:17:41 +0600

    dynamic data added for chef

--- src/Components/ChefSection/ChefSection.jsx
+++ src/Components/ChefSection/ChefSection.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SingleChef from '../SingleChef/SingleChef';
 
 const ChefSection = () => {
+
+  const [chefs, setChefs] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/chefs')
+    .then(res=>res.json())
+    .then(data=> setChefs(data))
+    .catch((err)=>{
+      console.log(err);
+    })
+  }, [])
+
   return (
     <div className="flex flex-col w-full my-5">
       <h3 className='w-full mb-10 text-4xl font-extrabold text-center'>Our Popular Chefs</h3>
       <div className="grid gap-2 place-items-center md:grid-cols-2 lg:grid-cols-3">
-        <SingleChef></SingleChef>
-        <SingleChef></SingleChef>
-        <SingleChef></SingleChef>
-        <SingleChef></SingleChef>
-        <SingleChef></SingleChef>
-        <SingleChef></SingleChef>
+        {
+          chefs.map(chef=> <SingleChef
+            key={chef.id}
+            chef={chef}
+          />)
+        }
       </div>
     </div>
   );
--- src/Components/SingleChef/SingleChef.jsx
+++ src/Components/SingleChef/SingleChef.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 
-import recepieImg1 from '../../assets/images/recepie_1.jpg'
-import recepieImg2 from '../../assets/images/recepie_2.jpg'
+import recepieImg1 from "../../assets/images/recepie_1.jpg";
+import recepieImg2 from "../../assets/images/recepie_2.jpg";
+import { FaThumbsUp } from "react-icons/fa";
 
-const SingleChef = () => {
+const SingleChef = ({ chef }) => {
+  const { chefPicture, chefName, yearsOfExperience, numberOfRecipes, likes } =
+    chef;
   return (
     <div className="my-5 shadow-xl card w-96 bg-base-100">
       <figure>
         <img
-          src={recepieImg1}
+          style={{ height: "400px", width: "100%" }}
+          src={chefPicture}
           alt="Recepie 1"
         />
       </figure>
       <div className="card-body">
         <h2 className="card-title">
-          Shoes!
-          <div className="badge badge-secondary">NEW</div>
+          {chefName}
+          <div className="badge badge-secondary">
+            {likes} <FaThumbsUp className="ml-2" />
+          </div>
         </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="justify-end card-actions">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+        <p>Working as a chef for about <span className="font-bold">{yearsOfExperience}</span> years.</p>
+        <div className="w-full card-actions">
+          <div className="w-full py-5 badge badge-outline">
+            Total Recipes: {numberOfRecipes}
+          </div>
+        </div>
+        <div className="items-center justify-center card-actions">
+          <button className="w-full btn btn-primary">View All</button>
         </div>
       </div>
     </div>
